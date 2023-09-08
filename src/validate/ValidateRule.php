d672b686c86188ee6056d92910db6012d34043e5
赵岩 <sieg@zimutech.com>
2023-09-08 17:39:11 +0800

    更新命名空间

--- src/validate/ValidateRule.php
+++ src/validate/ValidateRule.php
@@ -10,7 +10,7 @@
 // +----------------------------------------------------------------------
 declare(strict_types=1);
 
-namespace think\validate;
+namespace zimutech\validate;
 
 /**
  * Class ValidateRule
